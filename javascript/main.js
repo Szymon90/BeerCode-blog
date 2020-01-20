9ae24680d9374c158b8d00e90663a701ae6cb7de
Szymon Rogowski <rogowski90@gmail.com>
2020-01-20 16:36:02 +0100

    adding animation to reduce  wrapper , navigation in the haburger menu, creating a reset.sass file

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,16 @@
-// Active hamburger
+// Active hamburger menu 
 const btnHamburger = document.querySelector(".hamburgerMenu");
 const enteringMenu = document.querySelector(".enteringMenu");
 const xLogo = document.querySelector(".xLogo");
 const activeBackgroundBtn1 = document.querySelector(".activeBackgroundBtn1")
+const wrapper = document.querySelector(".wrapper")
 btnHamburger.addEventListener("click", () => {
-    enteringMenu.style.left = "0"
-    activeBackgroundBtn1.style.display ="block"
+    enteringMenu.style.left = "0";
+    wrapper.classList.remove("enlargenWrapper")
+    wrapper.classList.add("reductionWrapper");
 });
 xLogo.addEventListener("click", () => {
-    enteringMenu.style.left = "-100%"
-})
+    enteringMenu.style.left = "-100%";
+    wrapper.classList.remove("reductionWrapper");
+    wrapper.classList.add("enlargenWrapper")
+});
