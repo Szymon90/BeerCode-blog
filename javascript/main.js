2b4dea961f42c23ed05af5777776ea741c143645
Szymon Rogowski <rogowski90@gmail.com>
2020-02-03 16:41:13 +0100

    add section main

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,28 @@ const hamburger = document.querySelector(".hamburger");
 const sideMenu = document.querySelector(".sideMenu")
 const wrapper = document.querySelector(".wrapper")
 
-hamburger.addEventListener("click",()=>{
+hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("hamburger--active")
     sideMenu.classList.toggle("sideMenu--active")
 })
+
+const beerInGlasse = () => {
+    let beerContaine = document.querySelectorAll(".beerContainer");
+    setTimeout(() => {
+        beerContaine[0].classList.add("beerContainerGrow")
+    }, 0)
+    setTimeout(() => {
+        beerContaine[1].classList.add("beerContainerGrow")
+    }, 1000)
+    setTimeout(() => {
+        beerContaine[2].classList.add("beerContainerGrow")
+    }, 2000)
+    setTimeout(() => {
+        beerContaine[3].classList.add("beerContainerGrow")
+    }, 3000)
+    setTimeout(() => {
+        beerContaine[4].classList.add("beerContainer5Grow")
+    }, 4000)
+}
+
+document.addEventListener("DOMContentLoaded", beerInGlasse());
